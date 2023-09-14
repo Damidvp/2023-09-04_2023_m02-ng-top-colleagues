780d9747d5e293b13a7558ef882bc6c2130dce83
Damien Mouchagues <damienmouchagues33@gmail.com>
2023-09-14 15:53:26 +0200

    Modification barre de navigation

--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -1,3 +1,4 @@
+import { Colleague } from './../models/colleague';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -24,11 +25,20 @@ export class AuthService {
 
   }
 
+  getUserLoggedIn(){
+    return this.http.get<Colleague>('https://app-005f27d8-9033-48cc-ba69-b798464dee52.cleverapps.io/api/v2/current_user');
+  }
+
   isLoggedIn(){
     console.log(localStorage.getItem("jwt"));
     if(localStorage.getItem("jwt")){
       return true;
     }
     return false;
+  }
+
+  logOut(){
+    localStorage.removeItem("jwt");
+    this.route.navigate(["/login"]);
   }
 }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,3 +1,5 @@
+import { Colleague } from './../../../models/colleague';
+import { AuthService } from './../../../providers/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+
+  userLogged:Colleague | null = null;
+
+  constructor(private authService:AuthService){
+    this.authService.getUserLoggedIn().subscribe((user) => {
+      this.userLogged = {
+        pseudo: user.pseudo,
+        first: user.first,
+        last: user.last,
+        score: user.score,
+        photo: user.photo
+      };
+    })
+
+  }
+
+  deconnexion(){
+    this.authService.logOut();
+  }
 
 }
